0bf3906701d17f99949eb2eb566b7dcc56b82f19
Alanna423 <alanna.zhang423@gmail.com>
2022-12-30 11:28:08 -0500

    Update

---
+++ Aimo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Aimo.Models;
+
+namespace Aimo.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Aimo.Models.Explore> Explore { get; set; } = default!;
+}
+
---
+++ Aimo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aimo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
